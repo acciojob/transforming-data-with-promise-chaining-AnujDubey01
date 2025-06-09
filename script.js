f4842491a6023de8cc2bde12d960097035364922
Anuj Dubey <149352591+AnujDubey01@users.noreply.github.com>
2025-06-10 00:51:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,40 +12,33 @@
 // 		})
 // })
 
-
-
 let numInput = document.getElementById("ip");
 let button = document.getElementById("btn");
 let output = document.getElementById("output");
 
 button.addEventListener("click", function () {
-    let value = Number(numInput.value); // Get input value as a number
+    let value = Number(numInput.value);
 
     if (numInput.value === "") {
         alert("Please enter a number.");
         return;
     }
-});
 
-
- // First Promise: Display initial number after 2 seconds
     new Promise(function (resolve, reject) {
         setTimeout(() => {
             output.innerText = "Result: " + value;
             resolve(value);
         }, 2000);
     })
-    // Second Promise: Multiply by 2 after 2 seconds
     .then(function (res) {
         return new Promise(function (resolve, reject) {
             setTimeout(() => {
                 let result = res * 2;
                 output.innerText = "Result: " + result;
                 resolve(result);
-            }, 2000);
+            }, 1000);
         });
     })
-    // Third Promise: Subtract 3 after 1 second
     .then(function (res) {
         return new Promise(function (resolve, reject) {
             setTimeout(() => {
@@ -55,7 +48,6 @@ button.addEventListener("click", function () {
             }, 1000);
         });
     })
-    // Fourth Promise: Divide by 2 after 1 second
     .then(function (res) {
         return new Promise(function (resolve, reject) {
             setTimeout(() => {
@@ -65,7 +57,6 @@ button.addEventListener("click", function () {
             }, 1000);
         });
     })
-    // Fifth Promise: Add 10 after 1 second
     .then(function (res) {
         return new Promise(function (resolve, reject) {
             setTimeout(() => {
